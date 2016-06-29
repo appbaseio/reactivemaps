3c6db0c11cb430902b49912feecb71dcf04e900a
Yash Shah <mail@yashshah.com>
2016-06-29 15:27:01 +0530

    Add styles

---
+++ Style.js
@@ -0,0 +1,31 @@
+module.exports = {
+    inputStyle: {
+        "border": `1px solid transparent`,
+        "borderRadius": `1px`,
+        "boxShadow": `0 2px 6px rgba(0, 0, 0, 0.3)`,
+        "boxSizing": `border-box`,
+        "MozBoxSizing": `border-box`,
+        "fontSize": `14px`,
+        "height": `32px`,
+        "marginTop": `27px`,
+        "outline": `none`,
+        "padding": `0 12px`,
+        "textOverflow": `ellipses`,
+        "width": `400px`,
+    },
+    divStatusStyle: {
+        position: 'absolute',
+        top: '15px',
+        right: '15px',
+    },
+    divAppbaseStyle: {
+        position: 'absolute',
+        width: '200px',
+        bottom: '15px',
+        left: '0px',
+        right: '0px',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        textAlign: 'center',
+    },
+};
