225117b122d507b33c78c4e44001e114d22d0d83
Yash Shah <mail@yashshah.com>
2016-07-01 23:04:05 +0530

    Implement List component

--- app/component/List.js
+++ app/component/List.js
@@ -1,6 +1,22 @@
 import { default as React, Component } from 'react'
 import { render } from 'react-dom'
 
+export class List extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    var itemsComponent = this.props.items.map(function (value, index) {
+      return <Item key={index} value={value} />;
+    });
+    return (
+      <div>
+        {itemsComponent}
+      </div>
+    );
+  }
+}
+
 class Item extends Component {
   constructor(props) {
     super(props);
