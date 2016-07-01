12e53cb1a13b56549117b32571662cdadfa7fba7
Yash Shah <mail@yashshah.com>
2016-07-01 23:07:20 +0530

    Create AppbaseList

---
+++ app/AppbaseList.js
@@ -0,0 +1,67 @@
+import { default as React, Component } from 'react'
+import { render } from 'react-dom'
+var Appbase = require('appbase-js');
+var helper = require('./helper.js')
+import {List} from './component/List.js'
+export class AppbaseList extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      selectedItems: [],
+      streamingStatus: 'Intializing..'
+    };
+    this.appbaseRef = helper.getAppbaseRef(this.props.config);
+    this.streamingInstance;
+  }
+  componentDidMount() {
+    this.getItems();
+  }
+  subscribeToUpdates() {
+    var requestObject = helper.getMatchAllQuery(this.props.config, this.props.fieldName, 1, true);
+    var self = this;
+    self.setState({
+      streamingStatus: 'Listening...'
+    });
+    if (this.streamingInstance) {
+      this.streamingInstance.stop();
+    }
+    this.streamingInstance = this.appbaseRef.searchStream(requestObject).on('data', function (stream) {
+      console.log(stream)
+      var updated = this.state.items;
+      updated.unshift(JSON.stringify(item._source));
+      this.setState({ items: updated });
+    }).on('error', function (error) {
+      console.log(error);
+    });
+  }
+
+  getItems() {
+    var requestObject = helper.getMatchAllQuery(this.props.config, this.props.fieldName, "1", false);
+    var self = this;
+    this.appbaseRef.search(requestObject).on('data', function (data) {
+      console.log(data)
+      self.addItemsToList(data.hits.hits)
+    }).on('error', function (error) {
+      console.log(error)
+    });
+  }
+  addItemsToList(newItems) {
+    var updated = this.state.items;
+    newItems.map( function (item) {
+      updated.push(JSON.stringify(item._source));
+    });
+    this.setState({ items: updated });
+  }
+
+  render() {
+    return (
+      <div>
+        <List items={this.state.items} />
+      </div>
+
+    );
+  }
+
+}
