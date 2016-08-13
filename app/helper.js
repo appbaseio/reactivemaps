ef5786fedca8e081e64c76f993d699cc9e8c50a9
Yash Shah <mail@yashshah.com>
2016-08-13 16:31:44 +0530

    Exporting the config in helper

--- app/helper.js
+++ app/helper.js
@@ -1,6 +1,8 @@
 var Appbase = require('appbase-js');
 export let appbaseRef;
+export let appbaseConfig;
 export function setConfigObject(config){
+	appbaseConfig = config.appbase;
 	appbaseRef = new Appbase({
 		url: 'https://scalr.api.appbase.io',
 		appname: config.appbase.appname,
