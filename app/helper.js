8213a44eb7d74f83d643fc18a26a02a93ce24b11
Yash Shah <mail@yashshah.com>
2016-08-13 13:26:16 +0530

    Fix helper function

--- app/helper.js
+++ app/helper.js
@@ -1,46 +1,6 @@
 var Appbase = require('appbase-js');
 
 module.exports = {
-	getRequestObject: function (config, fieldName, boundingBoxCoordinates, streaming) {
-    var geo_bounding_box = JSON.parse(`{"${fieldName}":` + JSON.stringify(boundingBoxCoordinates) + '}');
-		var _source = !streaming ?  `${fieldName}` : null;
-		return ({
-			type: config.appbase.type,
-			body: {
-				"size": 100,
-				"_source": [_source],
-				"query": {
-					"filtered": {
-						"query": {
-							"match_all": {}
-						},
-						"filter": {
-							geo_bounding_box
-						}
-					}
-				}
-			}
-		});
-	},
-	getMatchAllQuery: function(config, fieldName, pageNumber, size, streaming){
-		var _source = !streaming ?  `${fieldName}` : null;		
-		return ({
-			type: config.appbase.type,
-			body: {
-				"size": size,
-				"from": pageNumber*size,				
-				"_source": [_source],
-				"query": {
-					"filtered": {
-						"query": {
-							"match_all": {}
-						}
-					}
-				}
-			}
-		});
-	},
-
 	getAppbaseRef: function (config) {
 		return (
 			new Appbase({
