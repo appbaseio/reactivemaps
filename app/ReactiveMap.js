63e4665b80ec8b9ece44adf40ed06279fe1fe3d1
Yash Shah <mail@yashshah.com>
2016-08-13 16:30:39 +0530

    Set Immutable query config

--- app/ReactiveMap.js
+++ app/ReactiveMap.js
@@ -1,5 +1,6 @@
 import { default as React, Component } from 'react';
 import { render } from 'react-dom';
+import {queryObject} from './ImmutableQuery.js';
 var helper = require('./helper.js');
 
 export class ReactiveMap extends Component {
@@ -7,6 +8,7 @@ export class ReactiveMap extends Component {
   constructor(props) {
     super(props);
     helper.setConfigObject(this.props.config);
+    queryObject.setConfig(this.props.config.appbase);
   }
   render() {
     return false;
