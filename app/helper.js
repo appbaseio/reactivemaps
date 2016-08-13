0b0aa213f3752925bf60dc74a8c02b86f9159779
Yash Shah <mail@yashshah.com>
2016-08-13 14:42:59 +0530

    Refactor helper.js

--- app/helper.js
+++ app/helper.js
@@ -1,17 +1,10 @@
 var Appbase = require('appbase-js');
-let config;
-module.exports = {
-	setConfigObject: function(configObject){
-		config = configObject;
-	},
-	appbaseRef: function () {
-		return (
-			new Appbase({
-				url: 'https://scalr.api.appbase.io',
-				appname: config.appbase.appname,
-				username: config.appbase.username,
-				password: config.appbase.password
-    		})
-		);
-	},
+export let appbaseRef;
+export function setConfigObject(config){
+	appbaseRef = new Appbase({
+		url: 'https://scalr.api.appbase.io',
+		appname: config.appbase.appname,
+		username: config.appbase.username,
+		password: config.appbase.password
+	});
 };
