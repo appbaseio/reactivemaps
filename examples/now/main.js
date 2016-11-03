12a0ae85cff726a7ebf416ffd2a2625eecf063e5
Deepak Grover <deepakgrover333@gmail.com>
2016-11-03 19:33:35 +0530

    add earthquake demo; add shebang-loader; fix editorconfig and spacing

--- examples/now/main.js
+++ examples/now/main.js
@@ -1,148 +1,148 @@
 import { default as React, Component } from 'react';
 import {Img} from '../../app/sensors/component/Img.js';
 var ReactDOM = require('react-dom');
-import {ReactiveMap, 
-		AppbaseMap, 
-		AppbaseSearch, 
-		AppbaseSlider, 
-		AppbaseList} from '../../app/app.js';
+import {ReactiveMap,
+        AppbaseMap,
+        AppbaseSearch,
+        AppbaseSlider,
+        AppbaseList} from '../../app/app.js';
 
 class Main extends Component {
-	constructor(props) {
-	    super(props);
-	    this.cityQuery = this.cityQuery.bind(this);
-		this.categoryQuery = this.categoryQuery.bind(this);
-	}
-	cityQuery(value) {
-		if(value) {
-			let field = 'city';
-			let match = JSON.parse(`{"${field}":` + JSON.stringify(value) + '}');
-	    	return { match: match };
-    	} else return null;
-	}
-	categoryQuery(value) {
-		let query = {
-			bool: {
-				should: []
-			}
-		};
-		if(value && value.length) {
-			let field = 'category';
-			let queryBunch = value.map((val) => { return createMatchQuery(field, val); });
-			query = {
-				bool: {
-					should: queryBunch,
-					minimum_should_match: 1
-				}
-			};
-			return query;
-    	} else {
-    		return query;
-    	}
+    constructor(props) {
+        super(props);
+        this.cityQuery = this.cityQuery.bind(this);
+        this.categoryQuery = this.categoryQuery.bind(this);
+    }
+    cityQuery(value) {
+        if(value) {
+            let field = 'city';
+            let match = JSON.parse(`{"${field}":` + JSON.stringify(value) + '}');
+            return { match: match };
+        } else return null;
+    }
+    categoryQuery(value) {
+        let query = {
+            bool: {
+                should: []
+            }
+        };
+        if(value && value.length) {
+            let field = 'category';
+            let queryBunch = value.map((val) => { return createMatchQuery(field, val); });
+            query = {
+                bool: {
+                    should: queryBunch,
+                    minimum_should_match: 1
+                }
+            };
+            return query;
+        } else {
+            return query;
+        }
 
-    	function createMatchQuery(field, val) {
-    		let match = JSON.parse(`{"${field}":` + JSON.stringify(val) + '}');
-	    	return { match: match };
-    	}
-	}
-	popoverContent(marker) {
-		console.log(marker);
-		return (<div className="popoverComponent row">
-			<span className="imgContainer col s2">
-				<Img src={marker._source.photourl} />
-			</span>
-			<div className="infoContainer col s10">
-				<div className="nameContainer">
-					<strong>{marker._source.username}</strong>
-				</div>
-				<div className="description">
-					<p>is going to&nbsp;
-						<a href={marker._source.url} target="_blank">
-							{marker._source.venue}
-						</a>
-					</p>
-				</div>
-			</div>
-		</div>);
-	}
-	render() {
-		return (
-			<div className="row m-0 h-100">
-				<ReactiveMap config={this.props.config} />
-				<div className="col s12 m9 h-100">
-					<AppbaseMap
-						inputData={this.props.mapping.location}
-						defaultZoom={13}
-						defaultCenter={{ lat: 37.74, lng: -122.45 }}
-						historicalData={true}
-						markerCluster={false}
-						searchComponent="appbase"
-						searchField={this.props.mapping.venue}
-						mapStyle={this.props.mapStyle}
-						autoCenter={true}
-						searchAsMoveComponent={true}
-						title="Foursquare checkins"
-						showPopoverOn = "onClick"
-						popoverContent = {this.popoverContent}
-						depends={{
-							CitySensor: {"operation": "must", defaultQuery: this.cityQuery},
-							CategorySensor: {"operation": "must", defaultQuery: this.categoryQuery}
-						}}
-						/>
-				</div>
-				<div className="col s12 m3">
-					<div className="row h-100">
-						<div className="col s12">
-							<AppbaseList
-								sensorId="CitySensor"
-								inputData={this.props.mapping.city} 
-								defaultSelected="london"
-								showCount={true} 
-								size={1000} 
-								multipleSelect={false} 
-								includeGeo={false}
-								staticSearch={true}
-								title="Cities"
-								searchPlaceholder="Search City"
-							/>
-						</div>
-						<div className="col s12">
-							<AppbaseList
-								inputData={this.props.mapping.topic} 
-								sensorId="CategorySensor"
-								showCount={true}
-								size={100} 
-								multipleSelect={true} 
-								includeGeo={true} 
-								title="Categories"
-								depends={{
-									CitySensor: {"operation": "must", defaultQuery: this.cityQuery}
-								}}
-							/>
-						</div>
-					</div>
-				</div>
-			</div>
-		);
-	}
+        function createMatchQuery(field, val) {
+            let match = JSON.parse(`{"${field}":` + JSON.stringify(val) + '}');
+            return { match: match };
+        }
+    }
+    popoverContent(marker) {
+        console.log(marker);
+        return (<div className="popoverComponent row">
+            <span className="imgContainer col s2">
+                <Img src={marker._source.photourl} />
+            </span>
+            <div className="infoContainer col s10">
+                <div className="nameContainer">
+                    <strong>{marker._source.username}</strong>
+                </div>
+                <div className="description">
+                    <p>is going to&nbsp;
+                        <a href={marker._source.url} target="_blank">
+                            {marker._source.venue}
+                        </a>
+                    </p>
+                </div>
+            </div>
+        </div>);
+    }
+    render() {
+        return (
+            <div className="row m-0 h-100">
+                <ReactiveMap config={this.props.config} />
+                <div className="col s12 m9 h-100">
+                    <AppbaseMap
+                        inputData={this.props.mapping.location}
+                        defaultZoom={13}
+                        defaultCenter={{ lat: 37.74, lng: -122.45 }}
+                        historicalData={true}
+                        markerCluster={false}
+                        searchComponent="appbase"
+                        searchField={this.props.mapping.venue}
+                        mapStyle={this.props.mapStyle}
+                        autoCenter={true}
+                        searchAsMoveComponent={true}
+                        title="Foursquare checkins"
+                        showPopoverOn = "onClick"
+                        popoverContent = {this.popoverContent}
+                        depends={{
+                            CitySensor: {"operation": "must", defaultQuery: this.cityQuery},
+                            CategorySensor: {"operation": "must", defaultQuery: this.categoryQuery}
+                        }}
+                        />
+                </div>
+                <div className="col s12 m3">
+                    <div className="row h-100">
+                        <div className="col s12">
+                            <AppbaseList
+                                sensorId="CitySensor"
+                                inputData={this.props.mapping.city}
+                                defaultSelected="london"
+                                showCount={true}
+                                size={1000}
+                                multipleSelect={false}
+                                includeGeo={false}
+                                staticSearch={true}
+                                title="Cities"
+                                searchPlaceholder="Search City"
+                            />
+                        </div>
+                        <div className="col s12">
+                            <AppbaseList
+                                inputData={this.props.mapping.topic}
+                                sensorId="CategorySensor"
+                                showCount={true}
+                                size={100}
+                                multipleSelect={true}
+                                includeGeo={true}
+                                title="Categories"
+                                depends={{
+                                    CitySensor: {"operation": "must", defaultQuery: this.cityQuery}
+                                }}
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 Main.defaultProps = {
- 	mapStyle: "Blue Water",
- 	mapping: {
-		city: 'city',
-		topic: 'category.raw',
-		venue: 'venue_name_ngrams',
-		location: 'location'
-	},
-	config: {
-		"appbase": {
-			"appname": "checkin",
-		    "username": "6PdfXag4h",
-		    "password": "b614d8fa-03d8-4005-b6f1-f2ff31cd0f91",
-		    "type": "city"
-		}
-	}
+    mapStyle: "Blue Water",
+    mapping: {
+        city: 'city',
+        topic: 'category.raw',
+        venue: 'venue_name_ngrams',
+        location: 'location'
+    },
+    config: {
+        "appbase": {
+            "appname": "checkin",
+            "username": "6PdfXag4h",
+            "password": "b614d8fa-03d8-4005-b6f1-f2ff31cd0f91",
+            "type": "city"
+        }
+    }
 };
 
 ReactDOM.render(<Main />, document.getElementById('map'));
--- examples/transport/main.js
+++ examples/transport/main.js
@@ -1,136 +1,136 @@
 import { default as React, Component } from 'react';
 var ReactDOM = require('react-dom');
-import {ReactiveMap, 
-		AppbaseMap, 
-		AppbaseSearch, 
-		AppbaseSlider, 
-		AppbaseList} from '../../app/app.js';
+import {ReactiveMap,
+        AppbaseMap,
+        AppbaseSearch,
+        AppbaseSlider,
+        AppbaseList} from '../../app/app.js';
 
 class Main extends Component {
-	constructor(props) {
-	    super(props);
-	    this.topicDepends = this.topicDepends.bind(this);
-	    this.popoverContent = this.popoverContent.bind(this);
-	}
-	topicDepends(value) {
-		if(this.props.mapping.city && value) {
-			let match = JSON.parse(`{"${this.props.mapping.city}":` + JSON.stringify(value) + '}');
-	    	return { Match: match };
-    	} else return null;
-	}
-	popoverContent(marker) {
-		console.log(marker);
-		return (<div className="popoverComponent row">
-			<div className="infoContainer col s12">
-				<div className="nameContainer">
-					<strong>Bus: {marker._source.vid}</strong>
-				</div>
-				<div className="description">
-					<table>
-						<tr>
-							<th>
-								Destination
-							</th>
-							<td>
-								{marker._source.des}
-							</td>
-						</tr>
-						<tr>
-							<th>
-								Route
-							</th>
-							<td>
-								{marker._source.routes}
-							</td>
-						</tr>
-						<tr>
-							<th>
-								Delayed
-							</th>
-							<td>
-								{marker._source.dly+' '}
-							</td>
-						</tr>
-						<tr>
-							<th>
-								Zone
-							</th>
-							<td>
-								{marker._source.zone}
-							</td>
-						</tr>
-					</table>
-				</div>
-			</div>
-		</div>);
-	}
-	markerOnIndex(res) {
-		console.log(res);
-	}
-	render() {
-		return (
-			<div className="row m-0 h-100">
-				<ReactiveMap config={this.props.config} />
-				<div className="col s12 m6">
-					<div className="row h-100">
-						<div className="col s12 m6">
-							<AppbaseList
-								sensorId="RoutesSensor"
-								inputData={this.props.mapping.routes} 
-								defaultSelected="King Drive"
-								showCount={true} 
-								size={1000} 
-								multipleSelect={false} 
-								includeGeo={false}
-								staticSearch={true}
-								title="Routes"
-								searchPlaceholder="Search Routes"
-							/>
-						</div>
-					</div>
-				</div>
-				<div className="col s12 m6 h-100">
-					<AppbaseMap
-						inputData={this.props.mapping.location}
-						defaultZoom={13}
-						defaultCenter={{ lat: 37.74, lng: -122.45 }}
-						historicalData={true}
-						markerCluster={false}
-						searchComponent="appbase"
-						searchField={this.props.mapping.venue}
-						mapStyle={this.props.mapStyle}
-						autoCenter={true}
-						searchAsMoveComponent={true}
-						MapStylesComponent={true}
-						title="Chicago Transport"
-						showPopoverOn = "onClick"
-						popoverContent = {this.popoverContent}
-						markerOnIndex = {this.markerOnIndex}
-						streamAutoCenter={false}
-						depends={{
-							RoutesSensor: {"operation": "must"}
-						}}
-						/>
-				</div>
-			</div>
-		);
-	}
+    constructor(props) {
+        super(props);
+        this.topicDepends = this.topicDepends.bind(this);
+        this.popoverContent = this.popoverContent.bind(this);
+    }
+    topicDepends(value) {
+        if(this.props.mapping.city && value) {
+            let match = JSON.parse(`{"${this.props.mapping.city}":` + JSON.stringify(value) + '}');
+            return { Match: match };
+        } else return null;
+    }
+    popoverContent(marker) {
+        console.log(marker);
+        return (<div className="popoverComponent row">
+            <div className="infoContainer col s12">
+                <div className="nameContainer">
+                    <strong>Bus: {marker._source.vid}</strong>
+                </div>
+                <div className="description">
+                    <table>
+                        <tr>
+                            <th>
+                                Destination
+                            </th>
+                            <td>
+                                {marker._source.des}
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>
+                                Route
+                            </th>
+                            <td>
+                                {marker._source.routes}
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>
+                                Delayed
+                            </th>
+                            <td>
+                                {marker._source.dly+' '}
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>
+                                Zone
+                            </th>
+                            <td>
+                                {marker._source.zone}
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+        </div>);
+    }
+    markerOnIndex(res) {
+        console.log(res);
+    }
+    render() {
+        return (
+            <div className="row m-0 h-100">
+                <ReactiveMap config={this.props.config} />
+                <div className="col s12 m6">
+                    <div className="row h-100">
+                        <div className="col s12 m6">
+                            <AppbaseList
+                                sensorId="RoutesSensor"
+                                inputData={this.props.mapping.routes}
+                                defaultSelected="King Drive"
+                                showCount={true}
+                                size={1000}
+                                multipleSelect={false}
+                                includeGeo={false}
+                                staticSearch={true}
+                                title="Routes"
+                                searchPlaceholder="Search Routes"
+                            />
+                        </div>
+                    </div>
+                </div>
+                <div className="col s12 m6 h-100">
+                    <AppbaseMap
+                        inputData={this.props.mapping.location}
+                        defaultZoom={13}
+                        defaultCenter={{ lat: 37.74, lng: -122.45 }}
+                        historicalData={true}
+                        markerCluster={false}
+                        searchComponent="appbase"
+                        searchField={this.props.mapping.venue}
+                        mapStyle={this.props.mapStyle}
+                        autoCenter={true}
+                        searchAsMoveComponent={true}
+                        MapStylesComponent={true}
+                        title="Chicago Transport"
+                        showPopoverOn = "onClick"
+                        popoverContent = {this.popoverContent}
+                        markerOnIndex = {this.markerOnIndex}
+                        streamAutoCenter={false}
+                        depends={{
+                            RoutesSensor: {"operation": "must"}
+                        }}
+                        />
+                </div>
+            </div>
+        );
+    }
 }
 
 Main.defaultProps = {
- 	mapStyle: "MapBox",
- 	mapping: {
-		routes: 'routes.raw',
-		location: 'location'
-	},
-	config: {
-		"appbase": {
-			"appname": "bus",
-			"username": "UzOKXiRYK",
-			"password": "000fb7ce-c92e-4f78-b3b7-0d4e33964134",
-			"type": "vehicle"
-		}
-	}
+    mapStyle: "MapBox",
+    mapping: {
+        routes: 'routes.raw',
+        location: 'location'
+    },
+    config: {
+        "appbase": {
+            "appname": "bus",
+            "username": "UzOKXiRYK",
+            "password": "000fb7ce-c92e-4f78-b3b7-0d4e33964134",
+            "type": "vehicle"
+        }
+    }
 };
 
 ReactDOM.render(<Main />, document.getElementById('map'));
