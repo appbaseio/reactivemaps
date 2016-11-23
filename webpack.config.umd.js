fa836b801ed7c08ad6fc5737a38f4fb5fad0aac5
farhan687 <farhan687@gmail.com>
2016-11-23 17:17:46 +0530

    Build umd

--- webpack.config.umd.js
+++ webpack.config.umd.js
@@ -1,6 +1,7 @@
 var path = require('path');
 var LodashModuleReplacementPlugin = require('lodash-webpack-plugin');
 var webpack = require('webpack');
+var env = process.env.NODE_ENV;
 
 var umd_config = {
   entry: './app/app.js',
@@ -12,26 +13,30 @@ var umd_config = {
 
   module: {
     preLoaders: [
-      { test: /\.json$/, exclude: /node_modules/, loader: 'json' },
+        { test: /\.json$/, exclude: /node_modules/, loader: 'json'},
     ],
-    loaders: [{
-      test: /.jsx?$/,
-      loader: 'babel-loader',
-      exclude: /node_modules/,
-      query: {
-        presets: ['es2015', 'stage-0', 'react']
+    loaders: [
+      {
+        test: /.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015','stage-0', 'react']
+        }
+      },
+      {
+        test: /node_modules\/JSONStream\/index\.js$/,
+        loaders: ['shebang', 'babel']
       }
-    }, {
-      test: /node_modules\/JSONStream\/index\.js$/,
-      loaders: ['shebang', 'babel']
-    }]
+    ]
   },
 
   resolve: {
     alias: {
       react: path.resolve(__dirname, './node_modules/react'),
       'react-dom': path.resolve(__dirname, './node_modules/react-dom'),
-      'react-addons-transition-group': path.resolve(__dirname, './node_modules/react-addons-transition-group'),
+      'react-addons-transition-group':
+          path.resolve(__dirname, './node_modules/react-addons-transition-group'),
     },
   },
 
@@ -68,24 +73,26 @@ var umd_config = {
       shorthands: true
     }),
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': 'production',
+      'process.env.NODE_ENV': JSON.stringify(env),
     }),
   ],
 };
 
-umd_config.plugins.push(
-  new webpack.optimize.UglifyJsPlugin({
-    compressor: {
-      pure_getters: true,
-      unsafe: true,
-      unsafe_comps: true,
-      warnings: false,
-    },
-    output: {
-      comments: false,
-    },
-    sourceMap: false,
-  })
-);
+if (env === 'production') {
+  umd_config.plugins.push(
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        pure_getters: true,
+        unsafe: true,
+        unsafe_comps: true,
+        warnings: false,
+      },
+      output: {
+        comments: false,
+      },
+      sourceMap: false,
+    })
+  );
+}
 
 module.exports = umd_config;
