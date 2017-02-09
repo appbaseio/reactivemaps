224f70bbc894e384d82ae3e90b9fb6b40915b8e6
farhan687 <farhan687@gmail.com>
2017-02-09 15:21:34 +0530

    Combine & export reactivebase components

--- app/app.js
+++ app/app.js
@@ -3,10 +3,17 @@ import {ReactiveMap} from './actuators/ReactiveMap';
 import {GeoDistanceSlider} from './sensors/GeoDistanceSlider';
 import {GeoDistanceDropdown} from './sensors/GeoDistanceDropdown';
 import {PlacesSearch} from './sensors/PlacesSearch';
+import reactivebase from '@appbaseio/reactivebase';
 
-module.exports = {
+var combineObj = {
 	ReactiveMap: ReactiveMap,
 	GeoDistanceSlider: GeoDistanceSlider,
 	GeoDistanceDropdown: GeoDistanceDropdown,
 	PlacesSearch: PlacesSearch
 };
+
+for(let component in reactivebase) {
+	combineObj[component] = reactivebase[component];
+}
+
+module.exports = combineObj;
